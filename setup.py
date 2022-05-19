2a5da687d3d66bf2c8571b7257f84ce913ce5102
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2022-05-20 00:32:11 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrape_latest_user_agent',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
     install_requires=[
         'httpx',
